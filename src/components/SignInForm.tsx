a4dcdc4ed8d54a36f148687a87b0f74beb12cd2d
Karolina <marchewicz.karolina9@gmail,com>
2021-02-18 19:58:17 +0100

    zepsute

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -34,7 +34,7 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
     display: "flex",
-    flexDirection: "column",
+    flexDirection: "row",
     alignItems: "center",
   },
   logo: {
